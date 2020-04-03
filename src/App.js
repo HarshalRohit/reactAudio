ad4be7c9ff5cb2b35dd47708b89ad2fe2d7f4c7b
HarshalRohit <rohitharshal95@gmail.com>
2020-04-03 20:02:35 +0530

    change UI based on microphone permission

--- src/App.js
+++ src/App.js
@@ -11,17 +11,52 @@ class Button extends Component {
   };
 }
 
-class MicrophoneError extends Component {
-  
+
+class MainUI extends Component {
   render(){
-    if(this.props.audioStream)
-      return null;  
+    
+    if(this.props.microphoneAccess !== "granted")
+      return null;
 
     return (
       <div>
-        <span> Ohh!! Please allow microphone access. </span>
+        EVerything related to recording will appear here!!
+      </div>
+    );
+  };
+};
+
+class MicrophoneRequest extends Component {
+  render(){
+    
+    if(this.props.microphoneAccess !== "prompt")
+      return null;
+
+    return (
+      <div>
+        Access to microphone required.
+        Click to request access!!
+        <Button 
+          onClick={() => this.props.onClick()}
+          btnText="Request Permission" />
+
+      </div>
+    );
+  };
+}
+
+
+class MicrophoneError extends Component {
+  
+  render(){
+    if(this.props.microphoneAccess !== "denied")
+      return null;
+
+    return (
+      <div>
+        <span> You have denied access to microphone!!</span>
         <br />
-        <span> Displays onLoad, will fix it later </span>
+        <span> Please change it in site settings. </span>
       </div>
     )
   };
@@ -39,7 +74,7 @@ class App extends Component {
                   audioStream: null, 
                   chunks: []};
 
-    // this.getMicrophone = this.getMicrophone.bind(this);
+    this.getMicrophone = this.getMicrophone.bind(this);
     // this.handleRecordBtnClick = this.handleRecordBtnClick.bind(this);
     // this.handleStopBtnClick = this.handleStopBtnClick.bind(this);
     // this.mediaRecOnDataAvail = this.mediaRecOnDataAvail.bind(this);
@@ -50,7 +85,8 @@ class App extends Component {
 
   componentDidMount(){
 
-    const isMediaSupported = navigator.mediaDevices && navigator.mediaDevices.getUserMedia ? true : false;
+    const isMediaSupported = navigator.mediaDevices && 
+                navigator.mediaDevices.getUserMedia ? true : false;
 
     this.setState({
       isMediaSupported: isMediaSupported
@@ -60,28 +96,59 @@ class App extends Component {
 
       navigator.permissions.query({name:'microphone'})
         .then((result) => this.setState({microphoneAccess: result.state}));
-        /*.then(function(result) {
-
-
-
-          if (result.state === 'granted') {
-            // showMap();
-          } else if (result.state === 'prompt') {
-            // showButtonToEnableMap();
-          }
-        // Don't do anything if the permission was denied.
-        });*/
     }
     
   }
 
 
+  async getMicrophone() {
+    // will only be called when mediaSupported is true
+    // will accomodate that later
+    let stream = null;
+    try{
+      stream = await navigator.mediaDevices.getUserMedia({
+        audio: true
+      });
+
+      /*let someVar = navigator.permissions.query({name:'microphone'})
+              .then((result) => this.setState({microphoneAccess: result.state}));
+
+      console.log(someVar);*/
+
+      this.setState({ audioStream: new MediaRecorder(stream), 
+                      chunks: [],
+                      microphoneAccess: "granted"
+                    });
+
+      /*navigator.permissions.query({name:'microphone'})
+        .then((result) => this.setState({microphoneAccess: result.state}));*/
+
+      // this.state.audioStream = new MediaRecorder(stream);
+      // this.chunks = [];
+      // console.log(this.mediaRecorder);
+      // console.log(this.state.chunks);
+      // this.state.audioStream.ondataavailable = this.mediaRecOnDataAvail
+      // this.state.audioStream.onstop = this.mediaRecOnStop
+
+    } catch(err){
+      console.log(err);
+      this.setState({microphoneAccess: "denied"});
+    }  
+    
+  };
+
+
 
   render() {
-    
+    const { microphoneAccess } = this.state;
     return (
       <div className="App">
         <h1>Welcome!!</h1>
+        <MainUI microphoneAccess={microphoneAccess} />
+        <MicrophoneRequest 
+          microphoneAccess={microphoneAccess} 
+          onClick={this.getMicrophone} />
+        <MicrophoneError microphoneAccess={microphoneAccess} />
       </div>
     )
   }
