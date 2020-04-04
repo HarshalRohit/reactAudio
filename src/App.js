b1980afa177abb084a2f339d809930ba846c5d50
HarshalRohit <rohitharshal95@gmail.com>
2020-04-04 12:07:22 +0530

    moved recording related to mainUI component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 
 class Button extends Component {
   render() {
+
+    const disabled = this.props.disabled ? this.props.disabled: false;
     return (
-      <button type="button" onClick={this.props.onClick}>
+      <button 
+        type="button" 
+        onClick={this.props.onClick}
+        disabled={disabled} >
         {this.props.btnText}
       </button>
     );
@@ -13,6 +18,80 @@ class Button extends Component {
 
 
 class MainUI extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {audioStream: null, 
+                  chunks: [],
+                  recBtnDisable: false,
+                  stopBtnDisable: true
+                 };
+
+    this.setupAudioRecord = this.setupAudioRecord.bind(this);                  
+    this.mediaRecOnDataAvail = this.mediaRecOnDataAvail.bind(this);
+    this.mediaRecOnStop = this.mediaRecOnStop.bind(this);
+
+    this.handleRecordBtnClick = this.handleRecordBtnClick.bind(this);
+    this.handleStopBtnClick = this.handleStopBtnClick.bind(this);
+  }
+
+  componentDidMount(){
+
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    })
+    .then(this.setupAudioRecord)
+    .catch(() => {console.log("should not reach here!!")});
+    
+  }
+
+
+  setupAudioRecord(stream){
+
+    let audioStream = new MediaRecorder(stream);
+    audioStream.ondataavailable = this.mediaRecOnDataAvail
+    audioStream.onstop = this.mediaRecOnStop
+
+    this.setState({ audioStream: audioStream, 
+                      chunks: []
+                    });
+
+    // this.state.audioStream.ondataavailable = this.mediaRecOnDataAvail
+    // this.state.audioStream.onstop = this.mediaRecOnStop
+  }
+
+  mediaRecOnDataAvail(e){
+    const {chunks} = this.state;
+    // [...chunks, e.data]
+    this.setState({chunks:  chunks.concat([e.data])});
+    // console.log(this.state.chunks);
+  }
+
+  mediaRecOnStop(e){
+    const downloadLink = document.getElementById('download');
+   
+    downloadLink.href = URL.createObjectURL(new Blob(this.state.chunks));
+    downloadLink.download = 'acetest.wav';
+  }
+
+  handleRecordBtnClick(){
+    // this.state.audioStream.start();
+    // console.log("record button clicked");
+    this.state.audioStream.start();
+    this.setState({recBtnDisable: true,
+                   stopBtnDisable: false
+                 });
+  }
+
+  handleStopBtnClick(){
+    // console.log("stop button clicked");
+    this.state.audioStream.stop();
+    this.setState({recBtnDisable: false,
+                   stopBtnDisable: true
+                 });
+  }
+
   render(){
     
     if(this.props.microphoneAccess !== "granted")
@@ -20,7 +99,13 @@ class MainUI extends Component {
 
     return (
       <div>
-        EVerything related to recording will appear here!!
+        <a id="download">Download</a>
+        <Button btnText="Record" 
+                onClick={this.handleRecordBtnClick} 
+                disabled={this.state.recBtnDisable} />
+        <Button btnText="Stop" 
+                onClick={this.handleStopBtnClick}
+                disabled={this.state.stopBtnDisable} />
       </div>
     );
   };
@@ -70,15 +155,10 @@ class App extends Component {
 
     
     this.state = {isMediaSupported: false, 
-                  microphoneAccess: null, 
-                  audioStream: null, 
-                  chunks: []};
+                  microphoneAccess: null
+                 };
 
-    this.getMicrophone = this.getMicrophone.bind(this);
-    // this.handleRecordBtnClick = this.handleRecordBtnClick.bind(this);
-    // this.handleStopBtnClick = this.handleStopBtnClick.bind(this);
-    // this.mediaRecOnDataAvail = this.mediaRecOnDataAvail.bind(this);
-    // this.mediaRecOnStop = this.mediaRecOnStop.bind(this);
+    this.requestMicrophoneAccess = this.requestMicrophoneAccess.bind(this);
     
   };
 
@@ -95,48 +175,20 @@ class App extends Component {
     if(isMediaSupported){
 
       navigator.permissions.query({name:'microphone'})
-        .then((result) => this.setState({microphoneAccess: result.state}));
+        .then((result) => this.setState({microphoneAccess: result.state}))
+        // .then(() => this.requestMicrophoneAccess());
     }
     
   }
 
 
-  async getMicrophone() {
-    // will only be called when mediaSupported is true
-    // will accomodate that later
-    let stream = null;
-    try{
-      stream = await navigator.mediaDevices.getUserMedia({
-        audio: true
-      });
+  async requestMicrophoneAccess() {
 
-      /*let someVar = navigator.permissions.query({name:'microphone'})
-              .then((result) => this.setState({microphoneAccess: result.state}));
-
-      console.log(someVar);*/
-
-      this.setState({ audioStream: new MediaRecorder(stream), 
-                      chunks: [],
-                      microphoneAccess: "granted"
-                    });
-
-      /*navigator.permissions.query({name:'microphone'})
-        .then((result) => this.setState({microphoneAccess: result.state}));*/
-
-      // this.state.audioStream = new MediaRecorder(stream);
-      // this.chunks = [];
-      // console.log(this.mediaRecorder);
-      // console.log(this.state.chunks);
-      // this.state.audioStream.ondataavailable = this.mediaRecOnDataAvail
-      // this.state.audioStream.onstop = this.mediaRecOnStop
-
-    } catch(err){
-      console.log(err);
-      this.setState({microphoneAccess: "denied"});
-    }  
+    navigator.mediaDevices.getUserMedia({audio: true})
+      .then(() => {this.setState({microphoneAccess: "granted"})})
+      .catch(() => {this.setState({microphoneAccess: "denied"})}
     
   };
-
 
 
   render() {
@@ -147,7 +199,7 @@ class App extends Component {
         <MainUI microphoneAccess={microphoneAccess} />
         <MicrophoneRequest 
           microphoneAccess={microphoneAccess} 
-          onClick={this.getMicrophone} />
+          onClick={this.requestMicrophoneAccess} />
         <MicrophoneError microphoneAccess={microphoneAccess} />
       </div>
     )
